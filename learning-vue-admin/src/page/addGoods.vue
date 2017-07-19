8dd90c235bb2b086dea07134e21a8e95f3dc4bab
ykx <youkaixing@78dk.com>
2017-07-20 00:02:34 +0800

    添加食品详情（html部分）

--- learning-vue-admin/src/page/addGoods.vue
+++ learning-vue-admin/src/page/addGoods.vue
@@ -7,7 +7,7 @@
                 <el-form :model="categoryForm" ref="categoryForm" label-width="110px" class="form">
                     <el-row class="category_select">
                         <el-form-item label="食品种类">
-                            <el-select v-model="categoryForm.categorySelect"  style="width:100%">
+                            <el-select v-model="categoryForm.categorySelect" :placeholder="selectValue.label"  style="width:100%">
                                 <el-option v-for="item in categoryForm.categoryList" :key="item.value" :label="item.label" :value='item.value'></el-option>
                             </el-select>
                         </el-form-item>
@@ -29,26 +29,112 @@
                     <div class="add_category_button" @click="addCategoryFun">
                         <i class="el-icon-caret-top edit_icon" v-if="showAddCategory"></i>
                         <i class="el-icon-caret-bottom edit_icon" v-else slot="icon"></i>
+                        <span>添加食品种类</span>
                     </div>
                 </el-form>
+                <header class="form_header">添加食品</header>
+                <el-form :model="foodForm" :rules="foodrules" ref="foodForm" label-width="110px" class="form">
+                    <el-form-item label="食品名称" prop="name">
+                      <el-input v-model="foodForm.name"></el-input>
+                    </el-form-item>
+                    <el-form-item label="食品活动" prop="activity">
+                      <el-input v-model="foodForm.activity"></el-input>
+                    </el-form-item>
+                  <el-form-item label="食品详情" prop="description">
+                    <el-input v-model="foodForm.description"></el-input>
+                  </el-form-item>
+                  <!-- 食品图片-->
+                  <el-form-item label="上传食品图片">
+                    <el-upload class="avatar-uploader" :action="baseUrl + '/v1/addimg/food'" :show-file-list="false" :on-success="uploadImg" :before-upload="beforeImgUpload">
+                      <img v-if="foodForm.image_path" :src="baseImgPath + foodForm.image_path" alt="" class="avatar"/>
+                      <i v-else class="el-icon-plus avatar-uploader-icon"></i>
+                    </el-upload>
+                  </el-form-item>
+                  <el-form-item label="食品特点">
+                    <el-select :model="foodForm.attributes" multiple placeholder="请选择">
+                      <el-option v-for='item in foodForm.attributes' :model="item.value" :label="item.label" :key="item.value"></el-option>
+                    </el-select>
+                  </el-form-item>
+                  <el-form-item label="食品规格">
+                    <el-radio class="radio" v-model="foodSpecs" label="one">单规格</el-radio>
+                    <el-radio class="radio" v-model="foodSpecs" label="more">多规格</el-radio>
+                  </el-form-item>
+                  <el-row v-if="foodSpecs == 'one'">
+                    <el-form-item label="包装费">
+                      <el-input-number v-model="foodForm.specs[0].packing_fee" :min="0" :max="100"></el-input-number>
+                    </el-form-item>
+                    <el-form-item label="价格">
+                      <el-input-number v-model="foodForm.specs[0].price" :min="0" :max="10000"></el-input-number>
+                    </el-form-item>
+                  </el-row>
+                  <el-row v-else style="overflow: auto;text-align: center">
+                    <el-button type="primary" @click="dialogFormVisible = true" style="margin-bottom:10px">添加规格</el-button>
+                    <el-table :data="foodForm.specs" style="margin-bottom:20px" :row-class-name="tableRowClassName">
+                      <el-table-column prop="specs" label="规格"></el-table-column>
+                      <el-table-column prop="packing_fee" label="包装费"></el-table-column>
+                      <el-table-column prop="price" label="价格"></el-table-column>
+                      <el-table-column label="操作">
+                        <template scope="scope">
+                          <el-button size="small" type="danger" @click="handleDelete(scope.$index)">删除</el-button>
+                        </template>
+                      </el-table-column>
+                    </el-table>
+                  </el-row>
+                  <el-form-item>
+                    <el-button type="primary" @click="addFood('foodForm')">确认添加食品</el-button>
+                  </el-form-item>
+                </el-form>
+                <!-- 添加规格弹层-->
+                <el-dialog title="添加规格" v-model="dialogFormVisible">
+                  <el-form :rules="specsFormrules" :model="specsForm">
+                    <!-- 这里家prop需要进行验证-->
+                    <el-form-item label="规格" label-width="100px" prop="specs">
+                      <el-input v-model="specsForm.specs" auto-complete="off"></el-input>
+                    </el-form-item>
+                    <el-form-item label="包装费" label-width="100px">
+                      <el-input-number v-model="specsForm.packing_fee" :min="0" :max="100"></el-input-number>
+                    </el-form-item>
+                    <el-form-item label="价格" label-width="100px">
+                      <el-input-number v-mode="specsForm.price" :min="0" :max="10000"></el-input-number>
+                    </el-form-item>
+                  </el-form>
+                  <div slot="footer" class="dialog-footer">
+                    <el-button @click="dialogFormVisible = false">取 消</el-button>
+                    <el-button type="primary" @click="addspecs">确 定</el-button>
+                  </div>
+                </el-dialog>
             </el-col>
         </el-row>
     </div>
 </template>
 <script>
     import headTop from '@/components/header/headTop'
+    import { baseUrl,baseImgPath } from '@/config/env'
     import { getCategory,addCategory } from '@/api/getData'
     export default{
         data(){
             return {
+                //食品种类信息
                 categoryForm : {
                     categoryList : [],
-                    categorySelect : ''
+                    categorySelect : '',
+                    name : '',
+                    description : ''
                 },
-                showAddCategory : true,//显示添加添加属性块
+                //食品详情
+                foodForm : {
+                  image_path : '',
+                  name : '',
+                  description : '',
+                  activity : ''
+                },
+              foodrules : {
+                name: [
+                  {required : true,message : '请输入食品名称',trigger : 'blur'}
+                ]
+              },
+                showAddCategory : false,//显示添加添加属性块
                 restaurant_id : '',
-                name : '',
-                description : ''
             }
         },
         mounted(){
@@ -83,13 +169,18 @@
             //初始化数据
             this.initData();
         },
+    computed : {
+      selectValue:function(){//根据选择的optionj计算placeholder
+        return this.categoryForm.categoryList[this.categoryForm.categorySelect] || {}
+      }
+    },
         methods : {
             async initData(){
                 try{
                     const res = await getCategory(this.restaurant_id);
                     if(res.status == 1){
                         res.category_list.map((item,index) => {
-                            item.index = index;
+                            item.value = index;
                             item.label = item.name
                         })
                         this.categoryForm.categoryList = res.category_list;
@@ -128,20 +219,55 @@
                                 })
                             }
                         }catch(err){
-
+                          console.log(err)
                         }
 
+                    }else{
+                      this.$notify.error({
+                        title : '错误',
+                        message : "请检查输入是否正确",
+                        offset : 100
+                      })
+                      return false;
                     }
                 })
+            },
+            //上传图片
+            uploadImg(res,file){
+                if(res.stauts == 1){
+                  this.foodForm.image_path = res.image_path;
+                }else{
+                  this.$message({
+                    type : 'error',
+                    message : "上传图片失败"
+                  })
+                }
+            },
+          //  上传图片前判断
+          beforeImgUpload(file){
+            const isRightType = file.type === 'image/jpg' || file.type === 'image/png';
+            const isLt2M = file.size / 1024 /1024 < 2;
+            if(!isRightType){
+              this.$message.error('上传头像图片只能是 jpg/png 格式!');
             }
+            if(!isLt2M){
+              this.$message.error('上传头像图片大小不能超过 2MB!');
+            }
+            return isRightType && isLt2M;
+          }
         }
     }
 </script>
 <style lang="scss">
     @import "src/style/scss/mixin";
+    .form_header{
+      text-align: center;
+      margin-bottom:10px;
+    }
     .form{
         min-width: 400px;
         margin-bottom:30px;
+
         &:hover{
             box-shadow: 0 0 8px 0 rgba(232,237,250,.6),0 2px 4px 0 rgba(232,237,250,.5);
             border-radius: 6px;
@@ -159,6 +285,39 @@
              overflow: hidden;
              transition: all .4s;
              background-color: #f9fafc;
+         }
+         .showEdit{
+           height:185px;
+         }
+         .add_category{
+           background:#f9fafc;
+           padding:10px 30px 0px 10px;
+           border:1px solid #eaeefb;
+           border-top:none;
+         }
+         .add_category_button{
+           text-align: center;
+           line-height:40px;
+           border-bottom-left-radius: 6px;
+           border-bottom-right-radius: 6px;
+           border:1px solid #eaeefb;
+           border-top:none;
+           transition: all .4s;
+            cursor:pointer;
+           &:hover{
+              background:#f9fafc;
+              span,.edit_icon{
+                color:#20a0ff;
+              }
+            }
+          span{
+            @include sc(14px,#999)
+            transition:all .4s;
+          }
+          .edit_icon{
+            color:#ccc;
+            transition: all .4s;
+          }
          }
     }
 
